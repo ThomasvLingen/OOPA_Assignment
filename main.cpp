c3be436c5142f4d833e2ada5c24a84f2940f3418
ThomasvLingen <thovali96@gmail.com>
2016-03-01 10:21:37 +0100

    refactor: get rid of useless test stuff
    This was left over from when I was testing some regex stuff. Should not
    have been committed along with the code dump.

--- main.cpp
+++ main.cpp
@@ -1,18 +1,5 @@
 #include <iostream>
 #include "CircuitParsing/Stage1Lexer.hpp"
-#include "CircuitUtility/StrUtil.hpp"
-
-#include <vector>
-#include <regex>
-
-#define DEBUG_MODE
-
-// using namespace std;
-
-// regex_match example
-#include <iostream>
-#include <string>
-#include <regex>
 
 using namespace std;
 
