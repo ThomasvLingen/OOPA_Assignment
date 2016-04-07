5abfb5845a6d76baa5a976217c278aa6e1cd6363
ThomasvLingen <thovali96@gmail.com>
2016-04-07 10:33:29 +0200

    refactor: clean main.cpp

--- main.cpp
+++ main.cpp
@@ -1,10 +1,4 @@
 #include <iostream>
-#include "CircuitParsing/Stage1Lexer.hpp"
-#include "CircuitParsing/Stage2Lexer.hpp"
-#include "CircuitParsing/CircuitParser.hpp"
-#include "Circuit/NodeFactory.hpp"
-#include "Circuit/Nodes/Node.hpp"
-#include "Circuit/Circuit.hpp"
 #include "Circuit/CircuitVisitors/CircuitStatsVisitor.hpp"
 #include "Circuit/CircuitVisitors/UnconnectedVisitor.hpp"
 #include "Circuit/CircuitVisitors/CircuitFeedbackVisitor.hpp"
@@ -12,8 +6,6 @@
 #include "Circuit/CircuitSimulator.hpp"
 
 using namespace std;
-using Circuit::NodeFactory;
-using Circuit::nodeType;
 
 void printUsage();
 
@@ -26,9 +18,9 @@ int main(int argc, char* argv[]) {
     Circuit::CircuitSimulator sim(
         argv[1],
         {
-                new CircuitVisitors::UnconnectedVisitor(),
-                new CircuitVisitors::CircuitFeedbackVisitor(),
-                new CircuitVisitors::CircuitStatsVisitor()
+            new CircuitVisitors::UnconnectedVisitor(),
+            new CircuitVisitors::CircuitFeedbackVisitor(),
+            new CircuitVisitors::CircuitStatsVisitor()
         }
     );
 
